2ccb4ee103dca3ea1728d961068368a39375a478
wakebit <32976842+wakebit@users.noreply.github.com>
2022-11-25 00:28:44 +0500

    chore(model): stricter code

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+final class User extends Authenticatable
 {
     /** {@inheritDoc} */
     protected $fillable = [
