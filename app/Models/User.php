d5fbf88f0f4ce511955732adfc963e301a9721bb
wakebit <32976842+wakebit@users.noreply.github.com>
2022-11-25 00:27:45 +0500

    chore(model): remove unnecessary doc-blocks, comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -10,32 +9,20 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    /** {@inheritDoc} */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+    /** {@inheritDoc} */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+    /** {@inheritDoc} */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
