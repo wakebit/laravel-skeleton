0eaa3b54837204da07807e0af36d19c47c8460ac
wakebit <32976842+wakebit@users.noreply.github.com>
2022-11-25 01:29:48 +0500

    feat(routes): remove web routes

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -23,9 +23,6 @@ final class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
         });
     }
 
