ad0df2478ebf5b4221377028eacbcd61a0fd4b21
wakebit <32976842+wakebit@users.noreply.github.com>
2022-11-27 00:15:10 +0500

    build(deps): add static analysis tool

---
+++ .phpstorm.meta.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PHPSTORM_META {
+    override(\Psr\Container\ContainerInterface::get(0), map([
+        '' => '@',
+    ]));
+
+    override(\Illuminate\Foundation\Application::make(0), map([
+        '' => '@',
+    ]));
+}
