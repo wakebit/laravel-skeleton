9c462bbd87aa22e3714092d7eec712e04b7718b5
wakebit <32976842+wakebit@users.noreply.github.com>
2022-11-27 00:37:32 +0500

    chore(exception): remove useless report callback, doc-blocks and replace with type hints

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,48 +3,29 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
+    /** {@inheritDoc} */
     protected $levels = [
         //
     ];
 
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<\Throwable>>
-     */
+    /** {@inheritDoc} */
     protected $dontReport = [
         //
     ];
 
-    /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
+    /** {@inheritDoc} */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
+    /** {@inheritDoc} */
+    public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        //
     }
 }
