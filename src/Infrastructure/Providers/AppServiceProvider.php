19aa777e600744f13b2e77dea846033001405747
wakebit <32976842+wakebit@users.noreply.github.com>
2026-01-02 02:25:54 +0500

    feat: change database migration stubs

--- src/Infrastructure/Providers/AppServiceProvider.php
+++ src/Infrastructure/Providers/AppServiceProvider.php
@@ -4,15 +4,29 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Providers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Database\Migrations\MigrationCreator;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
 {
-    /** {@inheritDoc} */
+    /**
+     * {@inheritDoc}
+     *
+     * @throws \InvalidArgumentException
+     */
     #[\Override]
     public function register(): void
     {
-        //
+        $this->app->extend(
+            'migration.creator',
+            static function (MigrationCreator $creator, Application $app): MigrationCreator {
+                return new MigrationCreator(
+                    $app->get(\Illuminate\Filesystem\Filesystem::class),
+                    $app->resourcePath('stubs'),
+                );
+            },
+        );
     }
 
     /**
